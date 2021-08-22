47e911862b86ec26ae7b6a6ef9a2840d41f8c043
Lysenko Oleg <moto.froloff@yandex.ru>
2021-08-23 01:47:14 +0300

    Logic and bad auto layout

--- SliderAndColor/ViewController.swift
+++ SliderAndColor/ViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var redLabel: UILabel!
+    
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var greenLabel: UILabel!
+    
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var blueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        mainView.layer.cornerRadius = 10
+        
+        redSlider.minimumTrackTintColor = .red
+        redSlider.value = 0
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redLabel.text = String(redSlider.value)
+        
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.value = 0
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenLabel.text = String(greenSlider.value)
+        
+        blueSlider.minimumTrackTintColor = .blue
+        blueSlider.value = 0
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueLabel.text = String(blueSlider.value)
     }
 
-
+    func changeColorOnMainView() {
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    @IBAction func redSliderAction() {
+        redLabel.text = String(redSlider.value)
+        changeColorOnMainView()
+        
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(greenSlider.value)
+        changeColorOnMainView()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(blueSlider.value)
+        changeColorOnMainView()
+    }
 }
-
